5aa0798c87f5afc922e016c9e94b4f43b58567c4
rahul paladugu <rahulpaladugu96@gmail.com>
2026-05-21 10:55:42 +1200

    vpc

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public.subnet_cidrs)
+  count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = var.az_names[count.index]
